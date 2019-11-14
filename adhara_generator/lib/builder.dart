734243787de7fb0e08803de369ded68c3eab504a
Rohit R. Abbadi <rohit@infito.com>
2019-11-14 22:21:10 +0530

    refactor

--- adhara_generator/lib/builder.dart
+++ adhara_generator/lib/builder.dart
@@ -2,5 +2,7 @@ import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:adhara_generator/src/model_generator.dart';
 
-Builder modeFactory(BuilderOptions options) =>
-    SharedPartBuilder([AdharaModelGenerator()], 'adhara_model');
+Builder modelFactory(BuilderOptions options){
+  print("creating a sharedPartBuilder for adhara models");
+  return SharedPartBuilder([AdharaModelGenerator()], 'adhara_model');
+}
